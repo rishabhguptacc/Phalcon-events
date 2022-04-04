b17acae5681065a89e38bd5d3591a8f131b30b79
Rishabh Gupta <rishabhgupta@cedcoss.com>
2022-04-04 18:29:03 +0530

    setting part is done.

--- src/app/controllers/SettingController.php
+++ src/app/controllers/SettingController.php
@@ -6,6 +6,43 @@ class SettingController extends Controller
 {
     public function indexAction()
     {
-        //return '<h1>Hello!!!</h1>';
+        $this->view->settingList = array('with_tag'=>"with tag", 'without_tag'=>"without tag");
+        // $this->view->settingList = array("with tag", "without tag");
+
     }
+
+    public function setSettingAction()
+    {
+        // $setting = new Settings;
+
+        $setting = Settings::findFirst();
+
+        $setting->assign(
+            $this->request->getpost(),
+            [
+                'title_optimization',
+                'default_price',
+                'default_stock',
+                'default_zipcode'
+            ]
+        );
+
+        // Store and check for errors
+        $success = $setting->save();
+
+        // passing the result to the view
+        $this->view->success = $success;
+
+        if ($success) {
+            $message = "Setting is saved!";
+        } else {
+            $message = "Sorry, the following problems were generated:<br>"
+                     . implode('<br>', $setting->getMessages());
+        }
+
+        // passing a message to the view
+        $this->view->message = $message;
+
+    }
+
 }
--- src/app/models/Settings.php
+++ src/app/models/Settings.php
@@ -7,6 +7,6 @@ class Settings extends Model
     public $setting_id;
     public $default_price;
     public $default_stock;
-    public $zipcode;
+    public $default_zipcode;
     public $title_optimization;
 }
